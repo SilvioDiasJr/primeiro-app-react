14326f7a30ae0e542603f2c18b325c7ddd529dfe
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-30 00:56:59 -0300

    adicionar importações de componentes e alterar estilização

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomePage from './components/HomePage'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
